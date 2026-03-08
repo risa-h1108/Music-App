ea8f9730c52c80c3f2a1d3d295087552f5f24cb3
risa-h1108 <tusen.takk39@gmail.com>
2026-03-08 21:39:15 +0900

    useEffectを用いてspotify clientをリファクタ

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import { getToken } from "./lib/spotify";
+import { useEffect } from "react";
+import SpotifyClient from "./lib/spotify";
 
 export default function App() {
-  getToken();
+  useEffect(() => {
+    async function init() {
+      const spotify = await SpotifyClient.inittialize();
+      spotify.test();
+    }
+    init();
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
       <main className="flex-1 p-8 mb-20">
