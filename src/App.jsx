794c521bcb6429dd59ef6896d8f27c0da7172bf9
risa-h1108 <tusen.takk39@gmail.com>
2026-03-11 02:48:59 +0900

    Spotify→itunes変更、データの取得＆表示_OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
 import { useEffect, useState } from "react";
-import SpotifyClient from "./lib/spotify";
+import ItunesClient from "./lib/itunes";
 import { SongList } from "./components/SongList";
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [popularSongs, setPopularSongs] = useState([]);
-  const [spotify, setSpotify] = useState(null);
 
-  //取得したデータを表示する
   useEffect(() => {
     async function init() {
       setIsLoading(true);
 
-      //初期化
-      const spotifyClient = await SpotifyClient.initialize();
-      setSpotify(spotifyClient);
+      const client = new ItunesClient();
 
-      //曲取得、Apiを叩いた結果(=getPopularSongs)をresultに格納する
-      const result = await spotifyClient.getPopularSongs();
-      console.log(result);
+      const result = await client.getPopularSongs();
 
-      //stateに保存
       setPopularSongs(result);
 
       setIsLoading(false);
     }
+
     init();
   }, []);
 
@@ -36,9 +30,40 @@ export default function App() {
         </header>
         <section>
           <h2 className="text-2xl font-semibold mb-5">Popular Songs</h2>
-          <SongList songs={popularSongs} />
+          <SongList isLoading={isLoading} songs={popularSongs} />
         </section>
       </main>
     </div>
   );
 }
+
+//Spotify用で書いたコード
+// import { useEffect, useState } from "react";
+// import SpotifyClient from "./lib/itunes";
+// import { SongList } from "./components/SongList";
+
+// export default function App() {
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [popularSongs, setPopularSongs] = useState([]);
+//   const [spotify, setSpotify] = useState(null);
+
+//   //取得したデータを表示する
+//   useEffect(() => {
+//     async function init() {
+//       setIsLoading(true);
+
+//       //初期化
+//       const spotifyClient = await SpotifyClient.initialize();
+//       setSpotify(spotifyClient);
+
+//       //曲取得、Apiを叩いた結果(=getPopularSongs)をresultに格納する
+//       const result = await spotifyClient.getPopularSongs();
+
+//       const popularSongs = result.items;
+//       console.log(popularSongs);
+
+//       setPopularSongs(popularSongs);
+//       setIsLoading(false);
+//     }
+//     init();
+//   }, []);
--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -11,28 +11,25 @@ export function SongList(props) {
 
   return (
     <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
-      {Array(20)
-        .fill()
-        .map(() => {
-          return (
-            <a
-              href="https://open.spotify.com/intl-ja/track/78W4mTLIh4qoLu92W4IQhO"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex-none cursor-pointer "
-            >
-              <img
-                alt="thumbnail"
-                src={
-                  "https://i.scdn.co/image/ab67616d0000b2738b7a8c1322028d45a8355f7a"
-                }
-                className="mb-2 rounded"
-              />
-              <h3 className="text-lg font-semibold">Song Name</h3>
-              <p className="text-gray-400">By Artist</p>
-            </a>
-          );
-        })}
+      {props.songs.map((song) => {
+        return (
+          <a
+            href={song.trackViewUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex-none cursor-pointer "
+            key={song.trackId}
+          >
+            <img
+              alt="thumbnail"
+              src={song.artworkUrl100}
+              className="mb-2 rounded"
+            />
+            <h3 className="text-lg font-semibold">{song.trackName}</h3>
+            <p className="text-gray-400">By {song.artistName}</p>
+          </a>
+        );
+      })}
     </div>
   );
 }
